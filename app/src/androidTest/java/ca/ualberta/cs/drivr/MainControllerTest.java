631bdcb3f7ec18fb421c84506eb88e5171c2185d
Adam Christiansen <achristi@ualberta.ca>
2016-10-12 18:20:41 -0600

    Added empty test classes for all classes

--- app/src/androidTest/java/ca/ualberta/cs/drivr/MainControllerTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/drivr/MainControllerTest.java
@@ -17,27 +17,17 @@
 
 package ca.ualberta.cs.drivr;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Created by adam on 2016-10-12.
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ca.ualberta.cs.drivr", appContext.getPackageName());
+public class MainControllerTest {
+    @Test
+    public void thisAlwaysPasses() {
+        assertEquals(12, 4 * 3);
     }
 }
