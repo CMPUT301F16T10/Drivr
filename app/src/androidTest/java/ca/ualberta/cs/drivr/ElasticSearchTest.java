ef8ffd2957afcde817eb7ec9c068f2e37b225a9e
coltonbegert <coltonbegert@gmail.com>
2016-10-23 23:19:10 -0600

    Created and added tests for the Elastic search backend, as well as the search string builder.

---
+++ app/src/androidTest/java/ca/ualberta/cs/drivr/ElasticSearchTest.java
@@ -0,0 +1,77 @@
+package ca.ualberta.cs.drivr;
+
+import android.net.ConnectivityManager;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotSame;
+
+/**
+ * Created by colton on 2016-10-23.
+ */
+
+public class ElasticSearchTest {
+    @Test
+    public void testRequestPost(){
+        Request request = new Request();
+        ElasticSearch elasticSearch = new ElasticSearch();
+        elasticSearch.requestPost(request);
+//        request.setRequestId(requestId);
+        Request loadedRequest = elasticSearch.loadRequest(request.getRequestId());
+        assertEquals(request, loadedRequest);
+    }
+
+    @Test
+    public void testRequestUpdate(){
+        Request request = new Request();
+        ElasticSearch elasticSearch = new ElasticSearch();
+        elasticSearch.requestPost(request);
+        Request loadedRequest = elasticSearch.loadRequest(request.getRequestId());
+        assertEquals(loadedRequest, request);
+        request.setDriver(new User("name", "email", "phonenumer").getPublicInfo());
+        elasticSearch.requestUpdate(request);
+        Request newLoadedRequest = elasticSearch.loadRequest(request.getRequestId());
+        assertNotSame(newLoadedRequest, loadedRequest);
+        assertEquals(newLoadedRequest, request);
+
+
+    }
+
+    @Test
+    public void testLoadRequest(){
+        Request request = new Request();
+        ElasticSearch elasticSearch =  new ElasticSearch();
+        elasticSearch.requestPost(request);
+        Request loadedRequest = elasticSearch.loadRequest(request.getRequestId());
+        assertEquals(request,loadedRequest);
+    }
+
+    @Test
+    public void testSaveUser(){
+        User user = new User();
+        ElasticSearch elasticSearch = new ElasticSearch();
+        elasticSearch.saveUser(user);
+//        elastic search will only update the userId if it successfully posts to the database
+        assertFalse(user.getUserId().isEmpty());
+    }
+
+//    This tests both the load and save functions
+    @Test
+    public void testLoadUser(){
+        User user = new User("John", "email@email", "123-456-7890");
+        ElasticSearch elasticSearch = new ElasticSearch();
+        elasticSearch.saveUser(user);
+        User loadedUser = elasticSearch.loadUser(user.getUserId());
+        assertEquals(loadedUser, user);
+
+
+    }
+
+    @Test
+    public void testOnNetworkStateChanged(){
+        //this needs some work
+
+    }
+}
---
+++ app/src/main/java/ca/ualberta/cs/drivr/SearchBuilder.java
@@ -0,0 +1,20 @@
+package ca.ualberta.cs.drivr;
+
+/**
+ * Created by colton on 2016-10-23.
+ */
+
+// This builds strings for the elasticsearch database to return
+//    There are different types of searched that can be made - location, points of interest ...
+public class SearchBuilder {
+
+//    search near a string
+    public String searchString(String searterm){
+        throw new UnsupportedOperationException();
+    }
+
+//    search near a geolocation
+    public String searchLocation(android.location.Location searchTerm ){
+        throw new UnsupportedOperationException();
+    }
+}
