454d1864231e527a97cd785b4a10fca6ac30879e
coltonbegert <coltonbegert@gmail.com>
2016-10-24 13:29:51 -0600

    fixed Elastic search tests

--- app/src/main/java/ca/ualberta/cs/drivr/ElasticSearch.java
+++ app/src/main/java/ca/ualberta/cs/drivr/ElasticSearch.java
@@ -1,5 +1,10 @@
 package ca.ualberta.cs.drivr;
 
+import android.net.ConnectivityManager;
+import android.net.LinkProperties;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -13,42 +18,58 @@ public class ElasticSearch {
     private ArrayList<Request> offlineRequests;
     private User user;
 
+
     public ArrayList<Request> requestSeach(String searchTerm){
         throw new UnsupportedOperationException();
     }
 
-//    method to post the requests to the elastic search database
-//    returns the reqeustId so that it can be found in the database
+    //    method to post the requests to the elastic search database
+    //    returns the reqeustId so that it can be found in the database
     public void requestPost(Request request){
         throw new UnsupportedOperationException();
-//        if post fails add request to offline requests
+        //        if post fails add request to offline requests
     }
 
-//    if the request.requestId is not null then you update instead of post
+    //    if the request.requestId is not null then you update instead of post
     public void requestUpdate(Request request){
         throw new UnsupportedOperationException();
-//        if updating fails due to network connection, post data to offlineRequests
+        //        if updating fails due to network connection, post data to offlineRequests
     }
 
-//    load the request for elastic search if the requestid is known
+    //    load the request for elastic search if the requestid is known
     public Request loadRequest(String requestId){
         throw new UnsupportedOperationException();
     }
 
     public void saveUser(User user) {
         throw new UnsupportedOperationException();
-//        if network fails, return null, this.user = user
-//
+        //        if network fails, return null, this.user = user
+
     }
 
-//    gets the user from the elastic search database
+    //    gets the user from the elastic search database
     public User loadUser(String userID){
         throw new UnsupportedOperationException();
     }
 
-//    When the user gets network connectivity, all offline cached data will be posted to the data basee
+    //    When the user gets network connectivity, all offline cached data will be posted to the data base
     public void onNetworkStateChanged(){
+//        ConnectivityManager connectivityManager;
+//        if (connectivityManager.getActiveNetworkInfo().isConnected()){
+//             //       we have network so push update
+//        }
         throw new UnsupportedOperationException();
 
+
+
+
+    }
+
+    public ArrayList<Request> getOfflineRequests() {
+        return offlineRequests;
+    }
+
+    public void setOfflineRequests(ArrayList<Request> offlineRequests) {
+        this.offlineRequests = offlineRequests;
     }
 }
