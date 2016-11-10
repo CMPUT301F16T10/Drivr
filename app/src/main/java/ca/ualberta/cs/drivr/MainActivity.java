5e7b1725ad4217aae73cf53ce933bbcecaa5141b
coltonbegert <coltonbegert@gmail.com>
2016-11-10 09:48:51 -0700

    created fab menu with library dependency

--- app/src/main/java/ca/ualberta/cs/drivr/MainActivity.java
+++ app/src/main/java/ca/ualberta/cs/drivr/MainActivity.java
@@ -168,14 +168,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             }
         });
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
     }
 
