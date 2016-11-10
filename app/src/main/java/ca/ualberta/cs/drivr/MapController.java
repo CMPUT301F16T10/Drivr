2a91f81dfc41e4906578d1a48560c5286dfb2dd6
justin widney <jwidney@ualberta.ca>
2016-11-09 17:11:42 -0700

    Added methods to map controller

--- app/src/main/java/ca/ualberta/cs/drivr/MapController.java
+++ app/src/main/java/ca/ualberta/cs/drivr/MapController.java
@@ -1,8 +1,15 @@
 package ca.ualberta.cs.drivr;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.model.Step;
+import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptor;
@@ -11,25 +18,31 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by justin on 09/11/16.
  */
-public class MapController {
+public class MapController implements DirectionCallback{
 
     private GoogleMap map;
     private Location myLocation;
-    private Location destination;
-    private Location pickupLocation;
+    private LatLng destinationLatLng;
+    private LatLng pickupLatlng;
     private Marker passedMarker;
     private Double myLatitude;
     private Double myLongitude;
+    private Context mContext;
 
     private GPSTracker gpsTracker;
-
+    private String serverKey = "AIzaSyB13lv5FV6dbDRec8NN173qj4HSHuNmPHE";
 
     public MapController(GoogleMap map, Context context) {
         // initialize map
+        this.mContext = context;
         this.map = map;
         gpsTracker = new GPSTracker(context);
         // initialize location
@@ -69,6 +82,14 @@ public class MapController {
 
     }
 
+    public void estimateCost(){
+        //int totalDistance = myLocation.distanceTo()
+        // float cost = totalDistance * 1.75
+    }
+
+
+
+
     // Call to add A Pick Up Marker
     public void addPickUp(LatLng latLng){
         MarkerOptions markerOptions = new MarkerOptions();
@@ -79,6 +100,38 @@ public class MapController {
         map.addMarker(markerOptions);
     }
 
+    // Send two Locations to Map and draw a route between them
+    public void addRequestOnMap(LatLng pickup, LatLng destination){
+
+        this.pickupLatlng = pickup;
+        this.destinationLatLng = destination;
+
+            GoogleDirection.withServerKey(serverKey)
+                    .from(pickup)
+                    .to(destination)
+                    .transitMode(TransportMode.DRIVING)
+                    .execute(this);
 
 
+    }
+
+
+    @Override
+    public void onDirectionSuccess(Direction direction, String rawBody) {
+        if (direction.isOK()) {
+            map.addMarker(new MarkerOptions().position(pickupLatlng).title("Pickup").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+            map.addMarker(new MarkerOptions().position(destinationLatLng).title("Destination").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+            ArrayList<LatLng> directionPositionList = direction.getRouteList().get(0).getLegList().get(0).getDirectionPoint();
+            map.addPolyline(DirectionConverter.createPolyline(mContext,directionPositionList,3, Color.RED));
+
+
+
+        }
+    }
+
+    @Override
+    public void onDirectionFailure(Throwable t) {
+
+    }
 }
