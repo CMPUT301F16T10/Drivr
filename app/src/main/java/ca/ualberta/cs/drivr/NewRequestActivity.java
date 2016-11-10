4103572534bd59b3b792b2a2a790f2f81437e3a1
Adam Christiansen <achristi@ualberta.ca>
2016-11-09 17:57:49 -0700

    Can enter source and destination locations in a new request

--- app/src/main/java/ca/ualberta/cs/drivr/NewRequestActivity.java
+++ app/src/main/java/ca/ualberta/cs/drivr/NewRequestActivity.java
@@ -1,12 +1,16 @@
 package ca.ualberta.cs.drivr;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
@@ -17,6 +21,7 @@ public class NewRequestActivity extends AppCompatActivity {
     public static final String EXTRA_PLACE = "ca.ualberta.cs.driver.NewRequestActivity.EXTRA_PLACE";
 
     private Place destinationPlace;
+    private Place sourcePlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +42,60 @@ public class NewRequestActivity extends AppCompatActivity {
         }
 
         // Show the destination information
-        if (destinationPlace != null) {
-            TextView textView = (TextView) findViewById(R.id.new_request_temp_text);
-            textView.setText(
-                    "Name: " + destinationPlace.getName() + "\n"
-                            + "Address: " + destinationPlace.getAddress() + "\n"
-                            + "Latitude: " + destinationPlace.getLatLng().latitude + "\n"
-                            + "Longitude: " + destinationPlace.getLatLng().longitude + "\n"
-            );
-        }
+        updateDestinationPlace(destinationPlace);
+
+        // Setup listener for selecting a new destination
+        final PlaceAutocompleteFragment destinationPAF =
+                (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.new_request_place_dest_autocomplete);
+        destinationPAF.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                updateDestinationPlace(place);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // Do nothing
+            }
+        });
+
+        // Setup listener for selecting a new source
+        final PlaceAutocompleteFragment sourcePAF =
+                (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.new_request_place_source_autocomplete);
+        sourcePAF.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                updateSourcePlace(place);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // Do nothing
+            }
+        });
+    }
+
+    /**
+     * Updates the text shown on screen for the destination place.
+     * @param place The new destination
+     */
+    private void updateDestinationPlace(Place place) {
+        destinationPlace = new ConcretePlace(place);
+        TextView name = (TextView) findViewById(R.id.new_request_place_dest_name);
+        name.setText(destinationPlace.getName());
+        TextView address = (TextView) findViewById(R.id.new_request_place_dest_address);
+        address.setText(destinationPlace.getAddress());
+    }
+
+    /**
+     * Updates the text shown on screen for the source (starting) place.
+     * @param place The new source
+     */
+    private void updateSourcePlace(Place place) {
+        sourcePlace = new ConcretePlace(place);
+        TextView name = (TextView) findViewById(R.id.new_request_place_source_name);
+        name.setText(sourcePlace.getName());
+        TextView address = (TextView) findViewById(R.id.new_request_place_source_address);
+        address.setText(sourcePlace.getAddress());
     }
 }
