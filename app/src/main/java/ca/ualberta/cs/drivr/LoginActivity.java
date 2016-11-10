49c46e65bbc80b5f7d818fea976ffe1bf11048e9
coltonbegert <coltonbegert@gmail.com>
2016-11-10 16:51:28 -0700

    login system works but does not implement elastic search to authenticate logins

--- app/src/main/java/ca/ualberta/cs/drivr/LoginActivity.java
+++ app/src/main/java/ca/ualberta/cs/drivr/LoginActivity.java
@@ -372,59 +372,7 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-//        return new CursorLoader(this,
-//                // Retrieve data rows for the device user's 'profile' contact.
-//                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-//                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-//
-//                // Select only email addresses.
-//                ContactsContract.Contacts.Data.MIMETYPE +
-//                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-//                .CONTENT_ITEM_TYPE},
-//
-//                // Show primary email addresses first. Note that there won't be
-//                // a primary email address if the user hasn't specified one.
-//                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-//    }
-//
-//    @Override
-//    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-//        List<String> emails = new ArrayList<>();
-//        cursor.moveToFirst();
-//        while (!cursor.isAfterLast()) {
-//            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-//            cursor.moveToNext();
-//        }
-//
-//        addEmailsToAutoComplete(emails);
-//    }
-//
-//    @Override
-//    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-//
-//    }
 
-//    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-//        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-//        ArrayAdapter<String> adapter =
-//                new ArrayAdapter<>(LoginActivity.this,
-//                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-//
-//        mEmailView.setAdapter(adapter);
-//    }
-
-
-//    private interface ProfileQuery {
-//        String[] PROJECTION = {
-//                ContactsContract.CommonDataKinds.Email.ADDRESS,
-//                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-//        };
-//
-//        int ADDRESS = 0;
-//        int IS_PRIMARY = 1;
-//    }
 
     /**
      * Represents an asynchronous login/registration task used to authenticate
@@ -504,13 +452,12 @@ public class LoginActivity extends AppCompatActivity {
 
     public class UserSignUpTask extends AsyncTask<Void, Void, Boolean> {
 
-        //        private final String mEmail;
-//        private final String mPassword;
+
         private final String username;
         private final String name;
         private final String email;
         private final String phone;
-        private User user;
+        private User newUser;
         private UserManager userManager = UserManager.getInstance();
 
         UserSignUpTask(String username, String name, String email, String phone) {
@@ -532,11 +479,11 @@ public class LoginActivity extends AppCompatActivity {
 //            if (!user.getUserId().isEmpty()){
 //                return false;
 //            }
-            user = new User();
+            newUser = new User();
 
             return true;
 
-
+        }
 
         @Override
         protected void onPostExecute(final Boolean success) {
@@ -550,7 +497,8 @@ public class LoginActivity extends AppCompatActivity {
                 user.setEmail(email);
                 user.setName(name);
                 user.setPhoneNumber(phone);
-//                user2 = user;
+                user.setUsername(username);
+
                 finish();
             } else {
 //                mPasswordView.setError(getString(R.string.error_incorrect_password));
