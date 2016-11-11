aead89d0ed900e115acc72533d68a0e06315009b
justin widney <jwidney@ualberta.ca>
2016-11-10 18:02:01 -0700

    Added the Views to the profileActivity

--- app/src/main/java/ca/ualberta/cs/drivr/ProfileActivity.java
+++ app/src/main/java/ca/ualberta/cs/drivr/ProfileActivity.java
@@ -19,6 +19,9 @@ package ca.ualberta.cs.drivr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Setting this up for JavaDocs.
@@ -26,11 +29,93 @@ import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
 
+
+    private TextView usernameTextView;
+    private TextView profileNameTextView;
+    private TextView emailTextView;
+    private TextView addressTextView;
+    private TextView numberTextView;
+    private ProfileController profileController;
+    private IUserManager iUserManager;
+
+    private ImageView profileBoxImageView;
+    private ImageView editProfileImageView;
+    private ImageView profilePictureImageView;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Initialize the Text Views
+        profileNameTextView = (TextView)findViewById(R.id.profile_name);
+        usernameTextView = (TextView)findViewById(R.id.profile_username);
+        emailTextView = (TextView)findViewById(R.id.profile_email);
+        addressTextView = (TextView)findViewById(R.id.profile_address);
+        numberTextView = (TextView)findViewById(R.id.profile_phone);
+
+        // Initialize the ImageViews
+        profileBoxImageView = (ImageView)findViewById(R.id.profile_box);
+        editProfileImageView = (ImageView)findViewById(R.id.editProfile);
+        profilePictureImageView = (ImageView)findViewById(R.id.profile_picture_image);
+
+        editProfileImageView.setClickable(true);
+        editProfileImageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+                public void onClick(View v) {
+                // Move To edit Mode
+            }
+        });
+
+        //Create Controllers
+        iUserManager = new IUserManager() {
+            @Override
+            public User getUser() {
+                // Query for User
+                return null;
+            }
+
+            @Override
+            public void setUser(User user) {
+
+            }
+
+            @Override
+            public RequestsList getRequestsList() {
+                return null;
+            }
+
+            @Override
+            public void setRequestsList(RequestsList requestsList) {
+
+            }
+
+            @Override
+            public UserMode getUserMode() {
+                return null;
+            }
+
+            @Override
+            public void setUserMode(UserMode userMode) {
+
+            }
+        };
+
+        profileController = new ProfileController(iUserManager);
+
+        // get user
+        // Set profile up
+
     }
+
+    // set the TextViews to the String Returned
+    public void InitializeProfile(){
+        throw new UnsupportedOperationException();
+    }
+
 
     public String getName() {
         throw new UnsupportedOperationException();
