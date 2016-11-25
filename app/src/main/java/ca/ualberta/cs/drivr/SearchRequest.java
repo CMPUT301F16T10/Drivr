876f7f4a00b999ab6b8c0d57331830d6e911c242
tiegan <tiegan@ualberta.ca>
2016-11-25 01:01:00 -0700

    ES/ESC test fixes, ES/ESC location addition, expansion of what we do in SearchRequests

--- app/src/main/java/ca/ualberta/cs/drivr/SearchRequest.java
+++ app/src/main/java/ca/ualberta/cs/drivr/SearchRequest.java
@@ -16,7 +16,7 @@ public class SearchRequest {
     private ArrayList<Request> requestList;
 
     private ConcretePlace location;
-    private String Keyword;
+    private String keyword;
 
     public SearchRequest(String minPrice, String maxPrice, String minPricePer, String maxPricePer, ConcretePlace location, String keyword) {
         this.minPrice = minPrice;
@@ -24,19 +24,43 @@ public class SearchRequest {
         this.minPricePer = minPricePer;
         this.maxPricePer = maxPricePer;
         this.location = location;
-        Keyword = keyword;
+        this.keyword = keyword;
+        requestList = new ArrayList<Request>();
     }
 
+    //TODO: When searches are in place just remove the return null
     public ArrayList<Request> getRequests(){
-        //TODO make actual array
-        return new ArrayList<>();
-//        throw new UnsupportedOperationException();
+        //return requestList;
+        return null;
     }
 
+    /**
+     * Reason for Location split (just temporary, remove this when done):
+     *
+     * US 04.05.01 (added 2016-11-14)
+     * As a driver, I should be able to search **by address** or **nearby an address**.
+     *
+     * One will use the ES SearchForLocationRequests to get all sourceAddresses with that location
+     *
+     * One will convert the location to geolocation and use ES SearchForGeolocationRequests to get
+     * all requests around that location
+     */
     private void SearchKeyword() {
         throw new UnsupportedOperationException();
     }
-    private void SearchLocation() {
+    private void SearchExactLocation() {
+        throw new UnsupportedOperationException();
+    }
+    //TODO: Get geolocation for location, use that in ElasticSearch SearchForGeolocationRequests
+    private void SearchNearLocation() {
+        throw new UnsupportedOperationException();
+    }
+
+    //Price filters are handled here (makes more sense)
+    private void FilterByPrice() {
+        throw new UnsupportedOperationException();
+    }
+    private void FilterByPricePer() {
         throw new UnsupportedOperationException();
     }
 }
